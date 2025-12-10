c58eced1ebf84eae6108b50d2dbf4757c081895f
Mohammed-Abuthahir <mohammedabuthahir214@gmail.com>
2025-12-10 20:04:29 +0530

    DSA

--- Strings/removeduplicates.java
+++ Strings/removeduplicates.java
@@ -2,20 +2,27 @@ import java.util.*;
 import java.util.Scanner;
 import java.util.Arrays;
 class removeduplicates{
-    public static void removed(String s){
-        LinkedHashSet<Character>set = new LinkedHashSet<>();
-        for(int i = 0;i<s.length();i++){
+    public static String removed(String s){
+       LinkedHashSet<Character> set = new LinkedHashSet<>();
+        for (int i = 0; i < s.length(); i++) {
             set.add(s.charAt(i));
         }
+        String str = "";
         for(char arr : set){
-            System.out.print(arr + " ");
+            String myString =  String.valueOf(arr);
+            str = myString + str;
         }
-        System.out.println();
+        String reverse = "";
+        for(int i = str.length()-1;i>=0;i--){
+            reverse = reverse + str.charAt(i);
+        }
+        return reverse;
     }
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter the String :");
         String s = scan.nextLine();
-        removed(s);
+        String result = removed(s);;
+        System.out.println("After remove duplicates :"+result);
     }
 }
